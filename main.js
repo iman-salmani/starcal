b37f41a98907c84a7849b3c7ea0643aa417fd5fe
iman <i.salmani@iran.ir>
2019-10-26 11:52:45 +0330

    page navigation

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ var downloadbutton = document.getElementById("download-button")
 var home = document.getElementById("home")
 var download = document.getElementById("download")
 
-homebutton.onclick = function () {setVisibleSlide(home);}
-downloadbutton.onclick = function () {setVisibleSlide(download);}
+homebutton.onclick = function () {changeCurrentMenuItem(homebutton);setVisibleSlide(home);}
+downloadbutton.onclick = function () {changeCurrentMenuItem(downloadbutton);setVisibleSlide(download);}
+
+function changeCurrentMenuItem(item) {
+    homebutton.classList.remove('current')
+    downloadbutton.classList.remove('current')
+
+    item.classList.add('current')
+}
 
 function setVisibleSlide(slide) {
     home.style.display = "none";
